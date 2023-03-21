1d9061c4087da1ab292def3cc64e1d7c0c407668
Aryan D <whales4fun@hotmail.com>
2023-03-20 21:25:53 -0700

    test

--- iot-lab-2/frontend_tutorial/wifi_server.py
+++ iot-lab-2/frontend_tutorial/wifi_server.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = "192.168.3.49" # IP address of your Raspberry PI
-PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
+HOST = "192.168.1.155" # IP address of your Raspberry PI
+PORT = 65433          # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
